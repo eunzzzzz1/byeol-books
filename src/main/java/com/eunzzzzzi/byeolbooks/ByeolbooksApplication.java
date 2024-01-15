5cfb3115a5a43deb0b2439a9fcdaad4231366c7e
eunzzzzz1 <eunzzzzz1@kakao.com>
2024-01-15 23:24:19 +0900

    :tada: Begin a Project

---
+++ src/main/java/com/eunzzzzzi/byeolbooks/ByeolbooksApplication.java
@@ -0,0 +1,13 @@
+package com.eunzzzzzi.byeolbooks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ByeolbooksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ByeolbooksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eunzzzzzi/byeolbooks/ByeolbooksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eunzzzzzi.byeolbooks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ByeolbooksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
