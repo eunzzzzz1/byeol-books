89175e804ab82550537df2284c20568ca57f6d0f
eunzzzzz1 <eunzzzzz1@kakao.com>
2024-02-26 17:14:21 +0900

    :wrench: (security/SecurityConfig.java) Spring Security h2 Console CSRF 면제 처리

--- src/main/java/com/eunzzzzzi/byeolbooks/security/SecurityConfig.java
+++ src/main/java/com/eunzzzzzi/byeolbooks/security/SecurityConfig.java
@@ -1,4 +1,22 @@
 package com.eunzzzzzi.byeolbooks.security;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests().requestMatchers("/**").permitAll()
+                .and()
+                .csrf().ignoringRequestMatchers("/h2-console/**");
+
+        return http.getOrBuild();
+    }
+
 }
